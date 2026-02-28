227744d0255c93725f526b90e8aa09e8c1bc1034
Vlad Mohoff <vladimir.o.mokhov@gmail.com>
2026-02-28 13:34:31 +0300

    add prettierrc and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import {defineConfig, globalIgnores} from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,20 +15,22 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       prettierPlugin.configs.recommended,
-      prettierConfig,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    rules: {
-      'no-console': 'warn',
-      'eqeqeq': 'warn',
-      'curly': 'warn',
-      'no-else-return': 'warn',
-
+    plugins: {
+      prettier: prettierPlugin
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+      ...prettierConfig.rules
+    },
   },
-])
+]);
